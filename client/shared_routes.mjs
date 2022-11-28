455f6efa5dc1824413c3ac48953c50671c9a191b
wenbing <zhengwenbing@me.com>
2022-11-28 13:41:24 +0800

    fix ts-ignore missing shared_internal_routes.js

--- client/shared_routes.mjs
+++ client/shared_routes.mjs
@@ -76,9 +76,9 @@ const importDefault = (item) => ({
 });
 const notfound = [_notfound].map(routeSourceToRegexp).map(importDefault)[0];
 async function getRoutes() {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   const { default: _routes } = await import(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     /* webpackMode: 'eager' */ "./shared_internal_routes.js"
   );
   return _routes.map(routeSourceToRegexp).map(importDefault);
